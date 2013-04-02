543357d8c48355fda156eebdb972b3e471079867
hoangna <minhnl@Minhs-MacBook-2.local>
2013-04-02 09:52:37 +0700

    Initial Commit

---
+++ DemoSlider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoSlider
+//
+//  Created by hoangna on 4/2/13.
+//  Copyright (c) 2013 HoangNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
